747b66206b3fdb3a5267a969efd3809abc491aba
rakib-netmow <rakib.netmow@gmail.com>
2023-12-27 10:20:17 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import grapesjs from "grapesjs";
+import "./App.css";
+import webpageplugin from "grapesjs-preset-webpage";
+import blockbasicPlugin from "grapesjs-blocks-basic";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [editor, setEditor] = useState(null);
+  useEffect(() => {
+    const editor = grapesjs.init({
+      container: "#editor",
+      fromElement: true,
+      // height: "100%",
+      // width: "100%",
+      showOffsets: true,
+      showRulers: true,
+      // storageManager: {
+      //   type: "local",
+      //   key: "gjs",
+      // },
+      plugins: [
+        webpageplugin,
+        blockbasicPlugin,
+        "grapesjs-plugin-forms",
+        "grapesjs-component-countdown",
+        "grapesjs-plugin-export",
+        "grapesjs-tabs",
+        "grapesjs-custom-code",
+        "grapesjs-touch",
+        "grapesjs-parser-postcss",
+        "grapesjs-tooltip",
+        "grapesjs-tui-image-editor",
+        "grapesjs-typed",
+        "grapesjs-style-bg",
+        "grapesjs-preset-webpage",
+      ],
+      pluginsOpts: {
+        webpageplugin: {},
+        blockbasicPlugin: {},
+      },
+    });
+    setEditor(editor);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="editor"></div>
     </div>
   );
 }
